f909f17a42f6a9676308309d1664f83c93ecf1c4
Phat Nguyen <manhphat1157@gmail.com>
2023-07-05 13:44:39 -0400

    midterm

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://manhphat1157:manhphat1157@cluster0.g3wtykt.mongodb.net/?retryWrites=true&w=majority"
+
+};
